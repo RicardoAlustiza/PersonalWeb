7786ce575d46631289e622395d10bceb811232c9
Ricardo Alustiza R <ricardo.alustiza@gmail.com>
2018-05-15 11:33:28 +0200

    Adding "ScrollTo" effects

---
+++ js/effects.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("ul").find("a").click(function(e) {
+        e.preventDefault();
+        var section = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(section).offset().top
+        });
+    });
+});
